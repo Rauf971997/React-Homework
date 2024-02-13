d8fdc8b2cf542d0839f831297d0943d83c477de2
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-02-13 16:25:04 +0400

    For further work

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
-import React from "react";
-import  ReactDOM  from "react-dom/client";
-import App from "./App";
-const container=document.querySelector('#root');
-const root=ReactDOM.createRoot(container);
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const rootContainer = document.querySelector('#root');
+
+if (rootContainer === null) throw new Error('Root container doesn\'t exists');
+
+const root = createRoot(rootContainer);
+
 root.render(
-    <React.StrictMode>
-        < App/>
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
